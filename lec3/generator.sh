b4e7bb6bec2c04cd4083f61854eb6b7909e5f578
peshkanavalnogo <playvladosik@gmail.com>
2022-10-14 15:32:02 +0300

    add some comments

--- lec3/generator.sh
+++ lec3/generator.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Our idea is to create a var called "name" to echo it to our new_script
+# called "gg.sh".
+# Var name contains file name with lower case letters.
+# In echo we rename file with this file but lower case.
+# Then we execute created script.
+
 ls > txt.txt
 
 while read -r x;
@@ -9,5 +15,5 @@ do
 done < txt.txt
 
 bash -c "bash gg.sh"
-bash -c "rm gg.sh"
+bash -c "rm gg.sh" # Just remove useless junk
 bash -c "rm TXT.TXT"
--- lec5/fifo.sh
+++ lec5/fifo.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# In pipeline we line by line print this line and after that we print bash command "date".
+
 cat $1 | while read y; do echo "$y"; bash -c "date"; echo ; done;
+
+# To test this code you need to open 2 terminal windows.
+# In one window you create fifo by command "mkfifo @yourname".
+# That you need to give this fifo some text or whatever ( you can use just ls > @yourname.)
+# So in other window you need to open this scripts by this command "bash fifo.sh @yourname".
