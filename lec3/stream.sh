f0960f30fd3210852ead37fff71eaf759f386309
peshkanavalnogo <playvladosik@gmail.com>
2022-10-20 01:12:12 +0300

    changed comments

--- lec3/stream.sh
+++ lec3/stream.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# In "my_func" we execute command "$1" in file "$2".
-# Also all the errors will be in file "$2".
-# So the "2>&1" means that stderr and stdout will both be in file "$2".
+# In "my_func" we execute command "$2" in file "$1".
+# Also all the errors will be in file "$1".
+# So the "2>&1" means that stderr and stdout will both be in file "$1".
 
 set -x
 my_func(){
