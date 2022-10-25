4fff07cfcd08bfbba17a162cd46da081d28aabcc
peshkanavalnogo <playvladosik@gmail.com>
2022-10-26 00:09:49 +0300

    lec6 is complete (kinda)

---
+++ lec6/comp_macros.cpp
@@ -0,0 +1,19 @@
+#define USE_GPU
+
+#if defined(USE_GPU) && defined(USE_FLOAT)
+int main(){
+    return 1;
+}
+#elif defined(USE_GPU) && !defined(USE_FLOAT)
+int main(){
+    return 2;
+}
+#elif !defined(USE_GPU) && defined(USE_FLOAT)
+int main(){
+    return 3;
+{
+#else
+int main(){
+    return 4;
+}
+#endif
